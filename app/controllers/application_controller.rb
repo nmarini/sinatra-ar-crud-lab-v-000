b35b44fbc3f29c7bf1f00ac7015e0c8cc735388b
Nicolas Marini <nmarini44@gmail.com>
2018-11-22 02:19:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
-  patch '/post/:id' do
+  patch '/posts/:id' do
 
     @post = Post.find_by_id(params[:id])
     @post.name = params[:name]
